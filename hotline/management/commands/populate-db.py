10e30b737171b5231888ca47640b3f5958543a0d
sannae <edoardo.sanna.es@gmail.com>
2022-01-05 20:42:52 +0000

    added business names and reference names in populate db script

--- hotline/management/commands/populate-db.py
+++ hotline/management/commands/populate-db.py
@@ -11,16 +11,54 @@ from django.core.management.base import BaseCommand
 from hotline.models import *
 
 def create_customers(amount):
+
+    customer_names = [
+        'ICT Edge',
+        'Edu Toys',
+        'Mindful Components Ltd',
+        'Spotter Spare',
+        'Aroma Duck',
+        'Dogsquipo',
+        'Chefworks',
+        'Vehiclehut Inc.',
+        'Banana Group',
+        'Coco-doodle-doo Brothers'
+    ]
+
+    reference_names = [
+        'Mitchel Maddox',
+        'Yasmin Rangel',
+        'Daisy Owen',
+        'Roshni Anthony',
+        'Summer Woodward',
+        'Eilidh Mcleod',
+        'Kristofer Morley',
+        'Isla-Rose Dudley',
+        'Muskaan Petty'
+    ]
+
     # Create <amount> customers
-    pass
+    for i in range(0,amount):
+        customer = customer.objects.create(
+            name=random.choice(customer_names),
+            reference=random.choice(reference_names),
+            reference_phone=str(random.randint(111111,999999))
+            reference_email=random.choice(reference_names)+'@mail.com'
+            address=random.choice(customer_addresses)
+            phone='+39 '+str(random.randint(1111111,9999999))
+            sw_contract = bool(random.getrandbits(1))
+            hw_contract = bool(random.getrandbits(1))
+        )
+        # Save in database
+        customer.save()
 
 class Command(BaseCommand):
     help = 'Populates the database with random generated data.'
 
     # Arguments 
-    def add_arguments(self, parser):
-        parser.add_argument('--amount', type=int, help='The number of orders that should be created.')
-        parser.add_argument('--days', type=int, help='The max amount of days in the past')
+    # def add_arguments(self, parser):
+    #    parser.add_argument('--amount', type=int, help='The number of orders that should be created.')
+    #    parser.add_argument('--days', type=int, help='The max amount of days in the past')
 
     # Create customers
 
@@ -28,32 +66,14 @@ class Command(BaseCommand):
 
     # Create technicians
 
-
     def handle(self, *args, **options):
-        customers = ticket.objects.all()
-        technicians = technician.objects.all()
-        products = product.object.all
+        # customers = ticket.objects.all()
+        # technicians = technician.objects.all()
+        # products = product.object.all
 
-
-        amount = options['amount'] if options['amount'] else 2500
-        days = options['days'] if options['days'] else 30
+        amount = options['amount'] if options['amount'] else 5
+        days = options['days'] if options['days'] else 2
         
-        for i in range(0, amount):
+        create_customers(amount)
 
-            # Order made that day
-            order = Order.objects.create(
-                customer=random.choice(customers),
-                product=random.choice(products),
-                status=random.choice(statuses)[0],
-            )
-            # Random day in the last N days
-            dt = pytz.utc.localize(datetime.now() - timedelta(days=random.randint(0, days)))
-            order.date_created = dt
-
-            # Save date in notes
-            order.notes = str(order.date_created)
-
-            # Save in database
-            order.save()
-
-        self.stdout.write(self.style.SUCCESS('Successfully populated the database with ' + str(amount) + ' orders!'))
+        self.stdout.write(self.style.SUCCESS('Successfully populated the database with ' + str(amount) + ' customers!'))
