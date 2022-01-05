ba8ea473fe6b3a4e33ba1e36b29fde87763cbce1
sannae <edoardo.sanna.es@gmail.com>
2022-01-05 21:21:50 +0000

    added create scripts for departments, technicians, products, statuses in populate-db script

--- hotline/management/commands/populate-db.py
+++ hotline/management/commands/populate-db.py
@@ -4,11 +4,13 @@
 # > (env)$ python manage.py populate_db --amount 1000
 # To create 1000 random orders
 
+import lorem
 import random
 from datetime import datetime, timedelta
 import pytz
 from django.core.management.base import BaseCommand
 from hotline.models import *
+from hotline.views import new_product
 
 def create_customers(amount):
 
@@ -48,7 +50,7 @@ def create_customers(amount):
 
     # Create <amount> customers
     for i in range(0,amount):
-        my_customer = customer.objects.create(
+        new_customer = customer.objects.create(
             name=random.choice(customer_names),
             reference=random.choice(reference_names),
             reference_phone=str(random.randint(111111,999999)),
@@ -59,7 +61,112 @@ def create_customers(amount):
             hw_contract = bool(random.getrandbits(1))
         )
         # Save in database
-        my_customer.save()
+        new_customer.save()
+
+def create_department(amount):
+
+    department_names = [
+        'Support',
+        'IT',
+        'Production',
+        'Logistics',
+        'Customer Service',
+        'Development',
+        'Quality Assurance',
+        'Sales'
+    ]
+
+    # Create <amount> departments
+    for i in range(0,amount):
+        new_department = department.objects.create(
+            name=random.choice(department_names)
+        )
+        # Save in database
+        new_department.save()
+
+def create_technician(amount):
+    first_names = [
+        'John',
+        'Jane',
+        'Mary',
+        'Bob',
+        'Sam',
+        'Sally',
+        'Tom',
+        'Alice',
+        'Peter',
+        'Paul'
+    ]
+    last_names = [
+        'Smith',
+        'Jones',
+        'Williams',
+        'Brown',
+        'Davis',
+        'Miller',
+        'Wilson',
+        'Moore',
+        'Taylor',
+        'Anderson'
+    ]
+    for i in range(0,amount):
+        new_technician = technician.objects.create(
+            first_name=random.choice(first_names),
+            last_name= random.choice(last_names),
+            office_phone = '+39 '+str(random.randint(1111111,9999999)),
+            mobile_phone = '+39 '+str(random.randint(1111111,9999999)),
+            email = random.choice(first_names)+'.'+random.choice(last_names)+'@mail.com',
+            department_id = random.choice(department.objects.all())
+        )
+        # Save in database
+        new_technician.save()
+
+def create_products(amount):
+    product_names = [
+        'Laptop',
+        'Desktop',
+        'Tablet',
+        'Smartphone',
+        'Printer',
+        'Scanner',
+        'Camera',
+        'Projector',
+        'Monitor',
+        'Speaker',
+        'Microphone'
+    ]
+
+    for i in range(0,amount):
+        new_product = product.objects.create(
+            name = random.choice(product_names),
+            department_id = random.choice(department.objects.all()),
+            description = lorem.sentence()
+        )
+        # Save in database
+        new_product.save()
+
+def create_statuses(amount):
+    status_names = [
+        'Pending',
+        'In Progress',
+        'Completed',
+        'Cancelled'
+    ]
+    colors = [
+        '#FF0000',
+        '#00FF00',
+        '#0000FF',
+        '#FFFF00',
+    ]
+
+    for i in range(0,amount):
+        new_status = status.objects.create(
+            name = random.choice(status_names),
+            color = random.choice(colors)
+        )
+        # Save in database
+        new_status.save()
+
 
 class Command(BaseCommand):
     help = 'Populates the database with random generated data.'
@@ -69,20 +176,18 @@ class Command(BaseCommand):
         parser.add_argument('--amount', type=int, help='The number of orders that should be created.')
         parser.add_argument('--days', type=int, help='The max amount of days in the past')
 
-    # Create customers
-
-    # Create products
-
-    # Create technicians
-
     def handle(self, *args, **options):
-        # customers = ticket.objects.all()
-        # technicians = technician.objects.all()
-        # products = product.object.all
 
         amount = options['amount'] if options['amount'] else 5
         days = options['days'] if options['days'] else 2
         
+        # In order of dependencies:
         create_customers(amount)
+        create_department(amount)
+        create_technician(amount)
+        create_products(amount)
+        create_statuses(amount)
+        # create_tickets
 
-        self.stdout.write(self.style.SUCCESS('Successfully populated the database with ' + str(amount) + ' customers!'))
+        self.stdout.write(self.style.SUCCESS('Successfully populated the database with ' + 
+        str(amount) + ' objects!'))
