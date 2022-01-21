bf3a51b6b56a1dbf79c73dc0e15af3310aecc899
Edoardo Sanna <edoardo.sanna.es@gmail.com>
2022-01-21 16:00:02 +0100

    Update tests.py

--- hotline/tests.py
+++ hotline/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# Check https://www.valentinog.com/blog/testing-django/#prelude-how-do-i-know-what-to-test for guidance
+# Check also https://coverage.readthedocs.io/en/6.2/ for code coverage
