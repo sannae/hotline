28975fc01f975379d43601eb7708e378b329ddf6
sannae <edoardo.sanna.es@gmail.com>
2022-01-23 21:06:56 +0000

    fixed typo in the requirements creation step

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,6 @@ from django.core.management.utils import get_random_secret_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
